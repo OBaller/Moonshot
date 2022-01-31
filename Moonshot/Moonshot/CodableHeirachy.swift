1a5f66de1f21fffa0e76614e258fc5ef5c2c3969
brightsummit.co <naseemoyebola@gmail.com>
2022-01-31 11:10:27 +0100

    coddle heirachy

--- Moonshot/Moonshot/CodableHeirachy.swift
+++ Moonshot/Moonshot/CodableHeirachy.swift
@@ -2,14 +2,32 @@
 //  CodableHeirachy.swift
 //  Moonshot
 //
-//  Created by IBE on 31/01/2022.
+//  Created by naseem on 31/01/2022.
 //
 
 import SwiftUI
 
 struct CodableHeirachy: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+
+            // more code to come
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+
+        }
     }
 }
 
@@ -17,4 +35,14 @@ struct CodableHeirachy_Previews: PreviewProvider {
     static var previews: some View {
         CodableHeirachy()
     }
+}
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
 }
