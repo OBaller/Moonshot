5b7ebf09602805823d0538976943a9b2e3b1bca2
brightsummit.co <naseemoyebola@gmail.com>
2022-02-02 13:57:33 +0100

    feat: ContentView_ SwitchLayout

---
+++ Moonshot/Moonshot/ListLayout.swift
@@ -0,0 +1,63 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by naseem on 02/02/2022.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        ScrollView {
+            List {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                        .padding()
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+        .navigationTitle("Moonshot")
+        .background(.darkBackground)
+        .preferredColorScheme(.dark)
+        .listRowBackground(Color.darkBackground)
+        .listStyle(.plain)
+    }
+}
+
+
+struct ListLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        ListLayout()
+    }
+}
